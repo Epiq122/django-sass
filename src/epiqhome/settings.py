55018a7bcf95e413502146540d5de12206a78234
Robert Gleason <l2ob122@gmail.com>
2024-07-21 21:12:50 -0700

    setup postgres db, added packages needed

--- src/epiqhome/settings.py
+++ src/epiqhome/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
-from decouple import config
-
+from decouple import config, Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,14 +9,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+try:
+    SECRET_KEY = config("DJANGO_SECRET_KEY")
+    DEBUG = config("DJANGO_DEBUG", cast=bool)
+except Exception as e:
+    print("Error loading environment variables:", e)
+    raise
 
-# SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() == "true"
-DEBUG = config("DJANGO_DEBUG", cast=bool)
-
-
-print("DEBUG", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [".railway.app"]
 if DEBUG:
@@ -74,6 +73,22 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=CONN_MAX_AGE,
+            conn_health_checks=True,
+        )
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
